af135e64c646285d21fb8186b3ba7e6f2a6c71b0
Yaakov Block <jacobblockhere@gmail.com>
2023-09-17 20:01:40 +0300

    level 17, found numbers.

--- levels/solver_17.py
+++ levels/solver_17.py
@@ -13,7 +13,21 @@ def solve(driver, last_password):
         WebDriverWait(driver, 10).until(lambda driver: driver.find_element(By.XPATH, '//*[@id="content"]/pre'))
         results = driver.find_element(By.XPATH, '//*[@id="content"]/pre').text.split()
         if len(results) == 0:
-            password += "#" # means it's a number, idk how to find those yet
+            # it's a number, we're gonna convert to hex after adding 5 or 10, and try again. if it's still empty, it's an error
+            for j in [5,10]:
+                driver.find_element(By.XPATH, '//*[@id="content"]/form/input[1]').send_keys(f'^$(printf %x $(expr {j} + $(cut -c {x+1}-{x+1} {build_webpass_path(17)})))')
+                driver.find_element(By.XPATH, '//*[@id="content"]/form/input[2]').click()
+                
+                WebDriverWait(driver, 10).until(lambda driver: driver.find_element(By.XPATH, '//*[@id="content"]/pre'))
+                results = driver.find_element(By.XPATH, '//*[@id="content"]/pre').text.split()
+                if len(results) != 0:
+                    letter = results[0][0]
+                    number = int(letter, 16) - j
+                    password += str(number)
+                    break
+            else:
+                raise RuntimeError(f"Could not find letter {x} of the password, seems to be a number but hex method isn't working.")
+            
         else:
             password += results[0][0]
         print("password: ", password, end = '\r')
--- simulators/sim_to_17.py
+++ simulators/sim_to_17.py
@@ -1,2 +1,10 @@
 import os
+import re
+import readline                     # for arrow keys and history
 
+while True:
+    query = input("Enter query: ").replace("/etc/natas_webpass/natas17", "testing_webpass_file")
+    if re.search(r'[;|&`\'"]', query):
+        print("Invalid query")
+        continue
+    os.system(f'grep -i "{query}" dictionary.txt')
