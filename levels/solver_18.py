911bed454bf94e325799fe7111363068941dcff7
jb <68179224+jacblo@users.noreply.github.com>
2023-09-18 10:48:46 +0000

    mid work, can't work with this codespace

---
+++ levels/solver_18.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    import sys
+    sys.path.append("..")
+
+from common import build_url, build_webpass_path
+
+def solve(driver=None, _=None):
+    pass
+
+if __name__ == "__main__":
+    solve()
